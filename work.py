a7ef0faa971817675c2bd13d9e86df9f2d2c1d7f
sveta <svetlana.r.fisher@gmail.com>
2021-01-10 01:18:06 +0700

    ready sprite

--- work.py
+++ work.py
@@ -44,24 +44,48 @@ pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
-screen.fill(pygame.Color("black"))
+# screen.fill(pygame.Color("black"))
 clock = pygame.time.Clock()
 pygame.display.flip()
 
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'img')
-# player_img = pygame.image.load(os.path.join(img_folder, ))
 
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        # self.image = pygame.Surface((50, 40))
+        game_folder = os.path.dirname(__file__)
+        img_folder = os.path.join(game_folder, 'img')
+        self.image = pygame.image.load(os.path.join(img_folder, 'wall.jpg')).convert()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        pass
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        # self.image = pygame.Surface((50, 40))
+        game_folder = os.path.dirname(__file__)
+        img_folder = os.path.join(game_folder, 'img')
+        self.image = pygame.image.load(
+            os.path.join(img_folder, 'Box.jpg')).convert()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        pass
 
 class Map:
-    def __init__(self, room1, room2):
+    def __init__(self, file_name, room_x, room_y):
         pygame.sprite.Sprite.__init__(self)
         # self.image = player_img
         # self.rect = self.image.get_rect()
         # self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.string = []
-        self.room1 = room1
-        self.room2 = room2
+        self.room_x = room_x
+        self.room_y = room_y
 
         # player = None
 
@@ -81,6 +105,23 @@ class Map:
             'player': 'images.jpg',
             'nothing': 'nothing.jpg'
         }
+        self.open_file(file_name)
+        # print(self.string)
+        self.sprites_map()
+
+    def sprites_map(self):
+        for y in range(MAP_Y):
+            for x in range(MAP_X):
+                # print(x, y, self.string[y + MAP_Y * self.room_y][x + MAP_X * self.room_x])
+                if self.string[y + MAP_Y * self.room_y][x + MAP_X * self.room_x] == '#':
+                    wall = Wall(ZOOM * x, ZOOM * y)
+                    wall_sprites.add(wall)
+                    all_sprites.add(wall)
+                    print('wall', x, y)
+                elif self.string[y + MAP_Y * self.room_y][x + MAP_X * self.room_x] == '&':
+                    box = Box(ZOOM * x, ZOOM * y)
+                    wall_sprites.add(box)
+                    all_sprites.add(box)
 
         # self.colors = ['black', 'white', 'blue', 'red']
 
@@ -95,30 +136,35 @@ class Map:
                     self.string[count].append(i)
         # print(string)
 
-    def draw(self):
+    # def draw(self):
 
         # print('1111111')
-        for x in range(MAP_X):
-            for y in range(MAP_Y):
-                self.image = pygame.image.load(os.path.join(img_folder, self.tile_images[self.symbols[x + MAP_X * self.room1][y + MAP_Y * self.room2]]))
-                self.rect = self.image.get_rect()
-                self.rect.center = (WIDTH / 2, HEIGHT / 2)
-                pygame.draw.rect(screen, pygame.image.load(
-                    os.path.join(img_folder, self.image),
-                    ((ZOOM * x, ZOOM * y), (ZOOM * (x + 1), ZOOM * (y + 1))),
-                    0))
+        # for x in range(MAP_X):
+        #     for y in range(MAP_Y):
+        #         print(x, y)
+        #         if self.string[y][x] == '#':
+        #             w = Wall(ZOOM * x, ZOOM * y)
+                    # self.image = pygame.image.load(os.path.join(img_folder, self.symbols[self.tile_images[x + (MAP_X * self.room_x)][y + (MAP_Y * self.room_y)]]))
+                    # pygame.draw.rect(screen, w.player_img,
+                    #     ((ZOOM * x, ZOOM * y), (ZOOM * (x + 1), ZOOM * (y + 1))),
+                    #     0))
 
         #     pygame.draw.line(screen, pygame.Color('red'), (x* 98, 0), (x * 98, 1078), 1)
         #     print(x, x* 98)
         #
         # pygame.draw.line(screen, pygame.Color('red'), (1469, 0), (1469, 1077), 1)
-        pygame.display.flip()
+        # pygame.display.flip()
 
-Mapp = Map(1, 1)
-Mapp.open_file('1.txt')
-Mapp.draw()
 all_sprites = pygame.sprite.Group()
-all_sprites.add(Mapp)
+wall_sprites = pygame.sprite.Group()
+box_sprites = pygame.sprite.Group()
+
+Mapp = Map('1.txt', 1, 0)
+# Mapp.open_file('1.txt')
+# Mapp.draw()
+
+
+# all_sprites.add()
 
 running = True
 while running:
@@ -130,7 +176,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Обновление
+    all_sprites.update()
+    # Рендеринг
+    screen.fill(pygame.Color('black'))
     all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
 pygame.quit()
